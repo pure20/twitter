9a32f409eb6b0b82e5c221dec096e90fb7cffc52
pure20 <pure_20@hotmail.com>
2015-04-01 22:40:59 +0700

    Change main routes and create html wrapper

---
+++ application/views/layout.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <title>Gomeeki</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta content='width=device-width, initial-scale=1.0, user-scalable=no' name='viewport' />
+</head>
+<body>
+    <div id="body">
+        <?php echo $main_content ?>
+    </div>
+</body>
+</html>
