26cf6f34bc21225f4f42b87c3f4de5897045b3b0
pure20 <pure_20@hotmail.com>
2015-04-02 23:29:40 +0700

    Beautify

--- application/views/history/index.php
+++ application/views/history/index.php
@@ -1,5 +1,5 @@
 <ul id="menu-item">
-    <li class="item"><a href="<?php echo base_url() ?>">Back</a></li>
+    <li class="item"><a href="<?php echo base_url() ?>"> &lt; BACK TO THE TWEETS</a></li>
     <?php foreach ($savedSearch as $search): ?>
         <li class="item"><a href="<?php echo base_url().'?q='.$search->search_text ?>"><?php echo htmlentities($search->search_text) ?></a></li>
     <?php endforeach ?>
